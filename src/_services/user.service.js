9c610bc6cb6c39c251051c3a985b55acb95a2eb7
Rahul Chaudhary <rchaudhary@VELOCIS.IN>
2019-10-17 17:00:20 +0530

    first commit

---
+++ src/_services/user.service.js
@@ -0,0 +1,109 @@
+import config from 'config';
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    register,
+    getAll,
+    getById,
+    update,
+    delete: _delete
+};
+
+function login(email, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    };
+
+    return fetch(`${config.apiUrl}/authenticate`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('user', JSON.stringify(user));
+
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users`, requestOptions).then(handleResponse);
+}
+
+function getById(id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function register(user) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    //console.log(requestOptions);
+    //console.log(`${config.apiUrl}/register`);
+    return fetch(`${config.apiUrl}/register`, requestOptions).then(handleResponse);
+}
+
+function update(user) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { ...authHeader(), 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    };
+
+    return fetch(`${config.apiUrl}/users/${user.id}`, requestOptions).then(handleResponse);;
+}
+
+// prefixed function name with underscore because delete is a reserved word in javascript
+function _delete(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader()
+    };
+
+    return fetch(`${config.apiUrl}/users/${id}`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        //console.log(response);
+        if(data.status === false){
+            const error = (data && data.status) || response.statusText;
+            return Promise.reject(data);
+        }else{
+            return data;
+        }
+        // if (!response.ok) {
+        //     if (response.status === 401) {
+        //         // auto logout if 401 response returned from api
+        //         logout();
+        //         location.reload(true);
+        //     }
+
+        //     const error = (data && data.status) || response.statusText;
+        //     
+        // }
+
+        
+    });
+}
